e96105748cd2c57022fe221b7d672ba669c1d566
ss-.- <steeparts@gmail.com>
2017-06-26 11:40:39 +0400

    First Commit

---
+++ ReactCalc/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactCalc
+{
+    class Calc
+    {
+        public Calc(int x) { X = x; }
+        public int X { get; private set; }
+        public static int Y { get; set; }
+        public string s { get { return $"{X + Y}"; } }
+    }
+}
---
+++ ReactCalc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var c = new Calc(1);
+            //c.X = 1;
+            Calc.Y = 1;
+            Console.WriteLine(c.s);
+
+            var d = new Calc(2);
+            //Calc.Y = 2;
+            //d.X = 2;
+            Console.WriteLine(d.s);
+
+            Console.ReadKey();
+
+        }
+    }
+}
