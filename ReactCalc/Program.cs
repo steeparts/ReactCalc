7d5b2143cc4e422536b513a6aedfb47119ddfa98
ss-.- <steeparts@gmail.com>
2017-06-27 08:42:08 +0400

    Ещё одна рабочая версия (Double)

--- ReactCalc/Program.cs
+++ ReactCalc/Program.cs
@@ -10,14 +10,14 @@ namespace ReactCalc
     {
         static void Main(string[] args)
         {
-            int x = 0;
-            int y = 0;
+            double x = 0;
+            double y = 0;
             var calc = new Calc();
 
             if (args.Length > 0)
             {
-                x = ToInt(args[0], 1000);
-                y = ToInt(args[1], 999);
+                x = ToDouble(args[0], 1000);
+                y = ToDouble(args[1], 999);
             }
             else
             {
@@ -25,26 +25,54 @@ namespace ReactCalc
 
                 Console.Write("Введите x -> ");
                 var strx = Console.ReadLine();
-                x = ToInt(strx, 100);
+                x = ToDouble(strx, 100);
 
                 Console.Write("Введите y -> ");
                 var stry = Console.ReadLine();
-                y = ToInt(stry, 99);
+                y = ToDouble(stry, 99);
 
                 #endregion
             }
 
-            var result = calc.Sum(x, y);
+            int c = -1;
+            do
+            {
+                Console.WriteLine("1. Сумма (x + y)");
+                Console.WriteLine("2. Разность (x - y)");
+                Console.WriteLine("3. Произведение (x * y)");
+                Console.WriteLine("4. Частное (x / y)");
+                Console.WriteLine("4. Квадратный корень Sqrt(x)");
+                Console.WriteLine("4. Кубический корень Croot(x)");
+                Console.WriteLine("0. Выход");
+                Console.Write("-> ");
+                var strc = Console.ReadLine();
 
-            Console.WriteLine($"Сумма = {result}");
+                if (!int.TryParse(strc, out c))
+                    c = 0;
 
-            Console.ReadKey();                        
+                switch (c)
+                {
+                    case 0: break;
+                    case 1: Console.WriteLine($"{x} + {y} = {calc.Sum(x, y)}"); break;
+                    case 2: Console.WriteLine($"{x} - {y} = {calc.Dif(x, y)}"); break;
+                    case 3: Console.WriteLine($"{x} * {y} = {calc.Mul(x, y)}"); break;
+                    case 4:
+                        {
+                            if (y != 0)
+                                Console.WriteLine($"{x} / {y} = {calc.Div(x, y)}");
+                            else
+                                Console.WriteLine($"{x} / {y} = Ошибка деления на ноль");
+                        }; break;
+                    case 5: Console.WriteLine($"Sqrt({x}) = {calc.Sqrt(x)}"); break;
+                    case 6: Console.WriteLine($"Croot({x}) = {calc.CRoot(x)}"); break;
+                }
+            } while (c != 0);           
         }
 
-        private static int ToInt(string arg, int def = 100)
+        private static double ToDouble(string arg, double def = 100.0)
         {
-            int x;
-            if (!int.TryParse(arg, out x))
+            double x;
+            if (!double.TryParse(arg, out x))
             {
                 x = def;
             }
